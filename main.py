e46e0dfb362f7243c05ad3e6a0712fe4ae959e33
jjmin724 <151806540+jjmin724@users.noreply.github.com>
2025-05-21 12:30:55 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,15 @@ from data.processor import Preprocessor
 from data.graph_builder import GraphBuilder
 from tasks.pretrain import Pretrainer
 from models.trainer import Trainer
+from data.symbol_updater import SymbolUpdater
 from core.utils import ensure_dir
 
 def set_cuda():
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = ",".join(cfg["gpus"])
+
+def run_update_symbols():
+    SymbolUpdater(cfg).run()
 
 def run_collect():
     Collector(cfg).run()
@@ -31,14 +35,18 @@ def run_predict():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("mode", choices=["collect", "preprocess", "pretrain", "train", "predict"])
+    parser.add_argument("mode",
+        choices=["update_symbols", "collect", "preprocess", "pretrain", "train", "predict"])
     args = parser.parse_args()
 
     ensure_dir(cfg["workspace"])
     set_cuda()
 
-    {"collect": run_collect,
-     "preprocess": run_preprocess,
-     "pretrain": run_pretrain,
-     "train": run_train,
-     "predict": run_predict}[args.mode]()
+    {
+      "update_symbols": run_update_symbols,
+      "collect": run_collect,
+      "preprocess": run_preprocess,
+      "pretrain": run_pretrain,
+      "train": run_train,
+      "predict": run_predict
+    }[args.mode]()
